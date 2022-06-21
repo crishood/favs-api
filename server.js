663998f117b423b9ab40405d94c7d16f51259845
crishood <crishood13@gmail.com>
2022-06-21 13:38:35 -0500

    Create JWT authentication

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello ma G");
+});
+
 app.listen(port, () => {
   console.log("It's alive!");
 });
